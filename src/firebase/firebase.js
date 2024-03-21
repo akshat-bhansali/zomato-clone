b55f90e00611e00fde5a4477ad0647b1bc60faaf
akshatbhansali <akshatb2198@gmail.com>
2024-03-21 23:29:14 +0530

    initial

---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCRuJq9GxVWkrGJLibIAcB1cavpLOob_SY",
+  authDomain: "zomato-clone-417913.firebaseapp.com",
+  projectId: "zomato-clone-417913",
+  storageBucket: "zomato-clone-417913.appspot.com",
+  messagingSenderId: "368236534725",
+  appId: "1:368236534725:web:c080d4aa1204ab83f649ac",
+  measurementId: "G-M22HS35FT6"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+
+
+
+export { app, auth };
