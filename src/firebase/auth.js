187e7bff3cb6ebbc31e84aee259371903165ee21
akshatbhansali <akshatb2198@gmail.com>
2024-03-22 01:56:36 +0530

    connected firestore to auth

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -8,7 +8,9 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from "firebase/auth";
-
+import { getFirestore, addDoc, collection, getDocs, CollectionReference } from "firebase/firestore";
+import {db} from "./firebase" 
+const colletionRef = collection(db, 'user');
 export const doCreateUserWithEmailAndPassword = async (email, password) => {
   return createUserWithEmailAndPassword(auth, email, password);
 };
@@ -16,13 +18,39 @@ export const doCreateUserWithEmailAndPassword = async (email, password) => {
 export const doSignInWithEmailAndPassword = (email, password) => {
   return signInWithEmailAndPassword(auth, email, password);
 };
+const saveDataToFirestore = async (email,name) => {
+  const data = await fetchDataFromFirestore();
+  const existingUser = data.find(user => user.email == email);
+  console.log(existingUser);
+  if(!existingUser){
+    try {
+      const docRef = await addDoc(colletionRef, {
+        email: email,
+        name: name,
+        role: "user"
+      });
+    } catch (error) {
+      console.error("Error writing document: ", error);
+      alert("Error writing document to Database");
+    }
+  }
+};
+
+const fetchDataFromFirestore = async () => {
+  const db = getFirestore();
+  const querySnapshot = await getDocs(colletionRef);
+  const temporaryArr = [];
+  querySnapshot.forEach((doc) => {
+      temporaryArr.push(doc.data());
+  });
+    return temporaryArr;
+  };
 
 export const doSignInWithGoogle = async () => {
   const provider = new GoogleAuthProvider();
   const result = await signInWithPopup(auth, provider);
   const user = result.user;
-
-  // add user to firestore
+  saveDataToFirestore(user.email,user.displayName);
 };
 
 export const doSignOut = () => {
