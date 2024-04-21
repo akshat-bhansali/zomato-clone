528a7cf82b035793474fc0a50d8f5ed052029ab2
nybzmr <nybzmr02@gmail.com>
2024-04-21 15:56:29 +0530

    Added the footer and made about, contact, privacy policy, refund policy adn terms and conditions.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import ResLogin from "./components/restaurantAuth/reslogin";
 import ResRegister from "./components/restaurantAuth/resregister";
 
 import Header from "./components/header";
-import Home from "./components/home";
 
+import Home from "./components/home";
+import Footer from "./components/footer"
 import { AuthProvider } from "./contexts/authContext";
 import { useRoutes } from "react-router-dom";
 import Profile from "./components/profile";
 import RestaurantDetails from "./components/restaurantDetails";
 import Cart from "./components/cart";
 import Orders from "./components/order";
+import RefundPolicy from "./components/refund";
+import PrivacyPolicy from "./components/privacyPolicy";
+import ContactUs from "./components/contact";
+import AboutUs from "./components/about";
+import TermsAndConditions from "./components/tandc";
 
 function App() {
   const routesArray = [
@@ -55,12 +61,33 @@ function App() {
       path: "/restaurant/:id",
       element: <RestaurantDetails />,
     },
+    {
+      path: "/refund",
+      element: <RefundPolicy/>
+    },
+    {
+      path: "/privacypolicy",
+      element: <PrivacyPolicy/>
+    },
+    {
+      path: "/contact",
+      element: <ContactUs/>
+    },
+    {
+      path: "/tandc",
+      element: <TermsAndConditions/>
+    },
+    {
+      path: "/about",
+      element: <AboutUs/>
+    },
   ];
   let routesElement = useRoutes(routesArray);
   return (
     <AuthProvider>
       <Header />
-      <div className="w-full h-screen flex flex-col">{routesElement}</div>
+      <div className="w-full flex-grow flex flex-col">{routesElement}</div>
+      <Footer/>
     </AuthProvider>
   );
 }
