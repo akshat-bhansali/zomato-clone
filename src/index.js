4a9fd2cc979b11533af1fa3869821b9330858eb2
akshatbhansali <akshatb2198@gmail.com>
2024-06-19 04:56:00 +0530

    added analytics

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+  <Analytics>
+    <SpeedInsights><App /></SpeedInsights>
+    </Analytics>
   </BrowserRouter>
 );
